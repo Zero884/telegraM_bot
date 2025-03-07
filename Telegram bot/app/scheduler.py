abd1d5dbc410133e8d172eec171cf7ce29a63d6f
Zero884 <magmaman2011@gmail.com>
2025-03-07 21:20:17 +0300

    Update scheduler.py

--- Telegram bot/app/scheduler.py
+++ Telegram bot/app/scheduler.py
@@ -1,19 +1,19 @@
 import os
-import asyncio
-from telegram import Bot
+from aiogram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from app.quotes import get_random_quote
+from app.database import get_random_quote
 
 CHAT_ID = os.getenv("CHAT_ID")
 
-def schedule_daily_quote(app):
-    async def send_daily_quote():
-        if not CHAT_ID:
-            print("CHAT_ID не встановлено. Не можу відправити цитату.")
-            return
-        quote = get_random_quote()
-        await app.bot.send_message(chat_id=CHAT_ID, text=quote)
 
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_daily_quote, "cron", hour=9, minute=0)  
-    scheduler.start()
+scheduler = AsyncIOScheduler()
+
+
+async def send_daily_quote(bot: Bot, user_id:int):
+    """Надсилає цитату раз на день у вказаний чат."""
+    quote = get_random_quote()
+    bot.send_message(chat_id=user_id, text=quote)
+
+
+async def add_job_to_sched():
+    scheduler.add_job(send_daily_quote, "cron", hour=9, minute=0)
